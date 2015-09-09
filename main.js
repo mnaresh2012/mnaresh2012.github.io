c84c7b69a9224c806be6f07f6ce00f6295dfba05
naresh kumar <mnaresh2010@aol.com>
2015-09-09 14:58:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var App = angular.module('myApp', []);
 
 App.controller('MainController', function ($scope) {
   $scope.app = {
+    name: 'mr. naresh kumar',
+    occupation: 'UI Developer',
     email: ' mnaresh2010@aol.com',
     title: 'mr. naresh kumar',
     phone: '+91 900 801 8384'
